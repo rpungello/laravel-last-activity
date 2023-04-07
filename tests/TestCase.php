aa76174ca0628c1d313ccfc9cc75349cf2489bef
rpungello <rpungello@users.noreply.github.com>
2023-04-07 17:25:47 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Rpungello\LaravelLastActivity\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Rpungello\LaravelLastActivity\LaravelLastActivityServiceProvider;
 
